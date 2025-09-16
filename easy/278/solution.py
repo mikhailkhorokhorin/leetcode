0351928e5b072b0fcb94f160e66d908d916c0441
mikhailkhorokhorin <mikhailkhorokhorin@gmail.com>
2025-09-16 19:37:22 +0300

    Solve 278, 3280, 33 704, 153, 162 tasks

---
+++ easy/278/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        low, high = 0, n
+
+        while low < high:
+            mid = (low + high) // 2
+            if isBadVersion(mid):
+                high = mid
+            else:
+                low = mid + 1
+
+        return low
