bc7e749bcc380000c4dead089b1e2dfc488dc04e
mikhailkhorokhorin <mikhailkhorokhorin@gmail.com>
2025-10-12 10:16:44 +0300

    Solve 287 task

---
+++ medium/287/solution.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return num
+            seen.add(num)
+        return 0
