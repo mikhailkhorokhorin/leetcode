df497b8a630c450cf47e667743302e18c79956ae
mikhailkhorokhorin <mikhailkhorokhorin@gmail.com>
2025-11-25 19:57:39 +0300

    Solve 382 task

---
+++ medium/382/solution.py
@@ -0,0 +1,20 @@
+import random
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def __init__(self, head: Optional[ListNode]):
+        self.arr = []
+
+        while head:
+            self.arr.append(head.val)
+            head = head.next
+
+    def getRandom(self) -> int:
+        return random.choice(self.arr)
