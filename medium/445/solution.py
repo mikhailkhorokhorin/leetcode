7bba621740e8a66eaa8faae723d2933351048922
mikhailkhorokhorin <mikhailkhorokhorin@gmail.com>
2025-12-02 22:17:53 +0300

    Solve 445 task

---
+++ medium/445/solution.py
@@ -0,0 +1,36 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def addTwoNumbers(
+        self, l1: Optional[ListNode], l2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        s1, s2 = [], []
+
+        while l1:
+            s1.append(l1.val)
+            l1 = l1.next
+        while l2:
+            s2.append(l2.val)
+            l2 = l2.next
+
+        overflow = 0
+        head = None
+
+        while s1 or s2 or overflow:
+            x = s1.pop() if s1 else 0
+            y = s2.pop() if s2 else 0
+            total = x + y + overflow
+
+            new = ListNode(total % 10, head)
+            head = new
+
+            overflow = total // 10
+
+        return head
