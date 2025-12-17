96c9e53bd20327c94c1318ef58d91e1c3f8c55d0
mikhailkhorokhorin <mikhailkhorokhorin@gmail.com>
2025-12-17 19:45:43 +0300

    Solve 1669, 3760 tasks

---
+++ medium/1669/solution.py
@@ -0,0 +1,25 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeInBetween(
+        self, list1: ListNode, a: int, b: int, list2: ListNode
+    ) -> ListNode:
+        cur = list1
+        for _ in range(a - 1):
+            cur = cur.next
+
+        dummy = cur
+        for _ in range(b - a + 2):
+            dummy = dummy.next
+
+        cur.next = list2
+
+        while list2:
+            list2 = list2.next
+
+        list2.next = dummy
+        return list1
